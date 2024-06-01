56d52e335a61620dcc7aa595dc292bc090ca3a25
VietBac <81665443+vietbac0967@users.noreply.github.com>
2024-06-01 21:46:37 +0700

    init project

---
+++ Jenkinsfile
@@ -0,0 +1,25 @@
+pipeline {
+
+    agent any
+
+    tools {
+        maven 'my-maven'
+    }
+    stages {
+
+        stage('Build with Maven') {
+            steps {
+                sh 'mvn --version'
+                sh 'java -version'
+                sh 'mvn clean package -Dmaven.test.failure.ignore=true'
+            }
+        }
+
+    }
+    post {
+        // Clean after build
+        always {
+            cleanWs()
+        }
+    }
+}
---
+++ src/main/java/com/example/jenkinsdemo/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.jenkinsdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping()
+public class HelloController {
+    @GetMapping
+    public String getHello(){
+        return "Hello world " + LocalDate.now();
+    }
+}
